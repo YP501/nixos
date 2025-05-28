3371e9e3ada913b99c87100497ee52218c78acf2
YP501 <itzypboi@gmail.com>
2025-05-28 22:43:33 +0200

    please 🙏

--- disko-config.nix
+++ disko-config.nix
@@ -7,49 +7,27 @@
         content = {
           type = "gpt";
           partitions = {
-
             ESP = {
-              size = "512MiB";
+              name = "ESP";
+              start = "1M";
+              end = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
-                label = "EFI";
                 mountpoint = "/boot/efi";
-                mountOptions = [ "umask=0077" ];
+                options = [ "umask=0077" ];
               };
             };
 
-            swap = {
-              size = "32GiB";
-              type = "8200";
-              content = { type = "swap"; };
-            };
-
             root = {
+              name = "NIXROOT";
               size = "100%";
               content = {
-                type = "filesystem";
-                format = "btrfs";
-                label = "NIXROOT";
+                type = "btrfs";
+                extraArgs = [ "-f" ];
                 mountpoint = "/";
-                subvolumes = {
-                  "@" = {
-                    mountpoint = "/";
-                    type = "subvolume";
-                  };
-
-                  "@home" = {
-                    mountpoint = "/home";
-                    type = "subvolume";
-                  };
-
-                  "@var" = {
-                    mountpoint = "/var";
-                    type = "subvolume";
-                  };
-                };
-                mountOptions = [ "compress=zstd" "discard=async" ];
+                mountOptions = [ "compress=zstd" "noatime" ];
               };
             };
           };
